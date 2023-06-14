9ccddde4252340b1d69a37b3031c387f06ad6bf6
Massimiliano-Novello <massinov5@gmail.com>
2023-06-14 16:04:55 +0200

    paginazione

--- app/Http/Controllers/api/ProjectController.php
+++ app/Http/Controllers/api/ProjectController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index () {
-        $project = Project::all();
+        $project = Project::with(['type', 'technologies'])->paginate(5);
         return response()->json([
             'success' => true,
             'results' => $project
